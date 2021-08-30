dbfe5f59f9f718c44b8c7d9bdf1193692debabe4
Abhishek Sisodiya <39134683+abhishek-sisodiya@users.noreply.github.com>
2021-08-30 16:29:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
-var http = require('http');
+var http = require('http'),
+    express = require('express'),
 
 var app = express();
+app.use(cors());
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({'errors': {
+    message: err.message,
+    error: {}
+  }});
+});
 
 // finally, let's start our server...
 var server = app.listen( process.env.PORT || 3000, function(){
